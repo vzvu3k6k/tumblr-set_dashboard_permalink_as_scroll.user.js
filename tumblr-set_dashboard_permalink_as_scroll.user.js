f41110cdae835b673516f44467277b5e571dd2c5
vzvu3k6k <vzvu3k6k@gmail.com>
2016-09-19 09:59:46 +0900

    1.1

--- tumblr-set_dashboard_permalink_as_scroll.user.js
+++ tumblr-set_dashboard_permalink_as_scroll.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Tumblr: Set dashboard permalink as scroll
 // @description    Sets dashboard permalink to location bar as you scroll
-// @version        1.0
+// @version        1.1
 // @author         vzvu3k6k
 // @match          https://www.tumblr.com/dashboard*
 // @grant          none

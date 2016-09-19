bfe3fa899915040a82b2024a278df8ea0f537924
vzvu3k6k <vzvu3k6k@gmail.com>
2016-09-19 09:59:30 +0900

    Set grant to none

--- tumblr-set_dashboard_permalink_as_scroll.user.js
+++ tumblr-set_dashboard_permalink_as_scroll.user.js
@@ -4,38 +4,37 @@
 // @version        1.0
 // @author         vzvu3k6k
 // @match          https://www.tumblr.com/dashboard*
+// @grant          none
 // @namespace      http://vzvu3k6k.tk/
 // @license        CC0
 // ==/UserScript==
 
-location.href = 'javascript:(' + function(){
-  function replaceState(){
-    /* `Tumblr.fastDashboard.attached` has post nodes in and around visible area. */
-    var posts = Tumblr.fastDashboard.attached;
+function replaceState(){
+  // `Tumblr.fastDashboard.attached` has post nodes in and around visible area.
+  var posts = Tumblr.fastDashboard.attached;
 
-    var visiblePosts = _.chain(posts).select(function(i){
-      /* Check if the post is not `display: none` (for filtering userscript users) */
-      return i.offsetParent;
-    });
+  var visiblePosts = _.chain(posts).select(function(i){
+    // Check if the post is not `display: none` (for filtering userscript users)
+    return i.offsetParent;
+  });
 
-    var nearestPost = visiblePosts.min(function(i){
-      var post = i.querySelector('.post');
-      if(post){
-        return Math.abs(post.getBoundingClientRect().top);
-      }else{
-        return Infinity;
-      }
-    }).value();
-
-    if(nearestPost){
-      var postID = nearestPost.querySelector('.post').dataset.postId;
-      history.replaceState(null, null, '/dashboard/100/' + (+postID + 1));
+  var nearestPost = visiblePosts.min(function(i){
+    var post = i.querySelector('.post');
+    if(post){
+      return Math.abs(post.getBoundingClientRect().top);
     }else{
-      history.replaceState(null, null, '/dashboard/');
+      return Infinity;
     }
-  }
+  }).value();
 
-  /* j/k animated scroll takes `Tumblr.KeyCommands.scroll_speed` milliseconds */
-  var delay = ((window.Tumblr && Tumblr.KeyCommands && Tumblr.KeyCommands.scroll_speed) + 10) || 110;
-  document.addEventListener('scroll', _.debounce(replaceState, delay));
-} + ')()';
+  if(nearestPost){
+    var postID = nearestPost.querySelector('.post').dataset.postId;
+    history.replaceState(null, null, '/dashboard/100/' + (+postID + 1));
+  }else{
+    history.replaceState(null, null, '/dashboard/');
+  }
+}
+
+// j/k animated scroll takes `Tumblr.KeyCommands.scroll_speed` milliseconds.
+var delay = ((window.Tumblr && Tumblr.KeyCommands && Tumblr.KeyCommands.scroll_speed) + 10) || 110;
+document.addEventListener('scroll', _.debounce(_.throttle(replaceState, 500), delay));
